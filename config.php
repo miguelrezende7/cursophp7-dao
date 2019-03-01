8d51806ecb40165842809d1e2116e9fe5f3f7f34
miguelrezende7 <miguelrezende7@gmail.com>
2019-03-01 15:43:44 -0300

    iniciamos o projeto

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+
+spl_autoload_register(function($class_name){
+
+  $dirClass = "class";
+
+$filename = str_replace ("\\", "/", $dirClass . DIRECTORY_SEPARATOR . $class_name . ".php");
+
+
+    if (file_exists(($filename))){
+
+        require_once($filename);
+    }
+
+
+});
+
+
+?>
+
+
+
+
